7de7a3fc4da91e7162317c6ea3c159183b0d72e8
Tara <taraemonmornavich@gmail.com>
2026-08-30 13:20:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function sayHello() {
-    alert("Hello! ❤️");
-}
+    let score = 0;
+
+const scoreDisplay = document.getElementById("score");
+const clickButton = document.getElementById("clickButton");
+
+clickButton.addEventListener("click", function() {
+    score++;
+    scoreDisplay.textContent = score;
+});
