8d635b9c3b3581ba3353d8d8af85e8254d96c296
Tara <taraemonmornavich@gmail.com>
2026-09-03 22:41:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const clickSound = document.getElementById("clickSound");
 
+cookie.addEventListener("click", function() {
+    cookies++;
+    counter.textContent = "Cookies: " + cookies;
+
+    clickSound.currentTime = 0;
+    clickSound.play();
+});
